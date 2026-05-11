a21195171ed165738b90694a9122705de537ecca
Arnie Natasya <arnietasya1410@gmail.com>
2026-05-11 12:14:38 +0800

    Refactor login.php for improved security and layout

--- User/admin/login.php
+++ User/admin/login.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../security.php';
+require_once __DIR__ . '/../../security.php';
 ace_set_security_headers();
 ace_secure_session_start();
 require_once __DIR__ . '/_inc.php';
