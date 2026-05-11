2b64caaae651e26ca9a0c059978d9f4932a6b43c
Arnie Natasya <arnietasya1410@gmail.com>
2026-05-11 12:27:13 +0800

    Update cost_calculator_view.php

--- User/admin/cost_calculator_view.php
+++ User/admin/cost_calculator_view.php
@@ -50,7 +50,7 @@ function rm($v) {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>View Cost Calculator Record — ACE Admin</title>
-  <link rel="stylesheet" href="/User/front.css">
+  <link rel="stylesheet" href="/front.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
